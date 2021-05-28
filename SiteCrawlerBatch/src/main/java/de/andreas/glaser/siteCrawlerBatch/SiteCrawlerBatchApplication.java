75238dce981eef4571dcb1b9cba57552ac0b785b
Andreas Glaser <andreas.glaser@accenture.com>
2021-05-28 20:59:49 +0200

    batch scheduled

--- SiteCrawlerBatch/src/main/java/de/andreas/glaser/siteCrawlerBatch/SiteCrawlerBatchApplication.java
+++ SiteCrawlerBatch/src/main/java/de/andreas/glaser/siteCrawlerBatch/SiteCrawlerBatchApplication.java
@@ -1,19 +1,43 @@
 package de.andreas.glaser.siteCrawlerBatch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class SiteCrawlerBatchApplication {
+
+	@Autowired
+	JobLauncher jobLauncher;
+
+	@Autowired
+	Job job;
+
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 
 	public static void main(String[] args) {
-		System.exit(SpringApplication.exit(SpringApplication.run(SiteCrawlerBatchApplication.class, args)));
+		SpringApplication.run(SiteCrawlerBatchApplication.class, args);
+	}
+
+	@Scheduled(cron = "0 */1 * * * ?")
+	private void cron() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+		jobLauncher.run(job, new JobParameters());
 	}
 
 }
